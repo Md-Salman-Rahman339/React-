e8ef003e1c6e9f477bd3ef5d6341c0b9395278c8
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2024-03-22 02:30:58 +0600

    first commit

---
+++ vite+react/myapp/src/main.jsx
@@ -0,0 +1,89 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// // import App from './App.jsx'
+// // import './index.css'
+// // const index=0;
+
+
+// // setInterval(()=>{
+// //   const element=(
+// //     <h1 className='heading' tabIndex={index}>
+// //       <span className='text'>Hello {new Date().toLocaleTimeString()}</span>
+// //       <img src="" alt="" />
+// //     </h1>
+  
+// //   );
+// //   ReactDOM.render(element,document.getElementById('root'));
+
+
+// // },1000)
+
+
+// // import App from './App.jsx'
+// // import './index.css'
+// // const index=0;
+
+
+// // setInterval(()=>{
+// //   const element=(
+// //     <h1 className='heading' tabIndex={index}>
+// //       <span className='text'>Hello {new Date().toLocaleTimeString()}</span>
+// //       <img src="" alt="" />
+// //     </h1>
+  
+// //   );
+// //   ReactDOM.render(element,document.getElementById('root'));
+
+
+// // },1000)
+
+
+// function formatName(user) {
+//   return user.firstname + ' ' + user.lastname;
+// }
+
+// const user = {
+//   firstname: 'Salman Rahman',
+//   lastname: 'nayeem'
+// };
+
+// const element = (
+//   <h1>
+//     Hello {formatName(user)}
+//   </h1>
+// );
+
+// ReactDOM.render(element, document.getElementById('root'));
+
+
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+     document.getElementById('root')
+
+);
+
+// reportWebVitals();
+
+
+
+
+
+
+
+
+
+
+
+
+
